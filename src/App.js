09cfdbbebb8523070639f8a456969024b6535804
Paula <panna.na@gmail.com>
2023-03-28 11:28:47 -0400

    1 commit my portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
 
 function App() {
+  const [name, setName] = useState("Pawana(Paula)");
+  const [title, setTitle] = useState("Web Developer");
+  const [about, setAbout] = useState(
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo dui eget nisi euismod luctus. Aliquam tincidunt dolor mauris, eget gravida quam sodales et. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis ut ligula velit."
+  );
+
+  useEffect(() => {
+    document.title = `${name} | Portfolio`;
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>{name}</h1>
+        <h2>{title}</h2>
       </header>
+      <main>
+        <section>
+       
+          <h3>About Me</h3>
+          <p>{about}</p>
+        </section>
+            {/* <img className="img" src="pic-for-portfolio.jpg" width="200" height="200"></img> */}
+        <section>
+     
+          <h3>Skills</h3>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>React</li>
+          </ul>
+        </section>
+        <section>
+          <h3>Projects</h3>
+          <ul>
+            <li>
+              <a href="#">Project 1</a>
+            </li>
+            <li>
+              <a href="#">Project 2</a>
+            </li>
+            <li>
+              <a href="#">Project 3</a>
+            </li>
+          </ul>
+        </section>
+        <section>
+          <h3>Contact Me</h3>
+          <ul>
+            <li>Email: panna.na@gmail.com</li>
+            <li>Phone: 4436301829</li>
+            <li>
+              LinkedIn:{" "}
+              <a href="#">link to LinkedIn</a>
+            </li>
+          </ul>
+        </section>
+      </main>
     </div>
   );
 }
---
+++ src/components/PortfolioPic.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import myPhoto from './pic-for-portfolio.jpg';
+
+function MyPortfolio() {
+  return (
+    <div>
+      <img src={myPhoto} alt="My Photo" />
+    </div>
+  );
+}
+
+export default MyPortfolio;
